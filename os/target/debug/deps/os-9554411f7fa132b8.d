8ec2f676b6ad189f7c613270188d96fa101c4f84
halfpeop1e <halfpeop1e@outlook.com>
2026-05-09 19:07:38 +0800

    修改完成的可以编译通过的代码-23241248

---
+++ os/target/debug/deps/os-9554411f7fa132b8.d
@@ -0,0 +1,5 @@
+/mnt/os/target/debug/deps/os-9554411f7fa132b8.d: src/main.rs
+
+/mnt/os/target/debug/deps/os-9554411f7fa132b8: src/main.rs
+
+src/main.rs:
---
+++ os/target/riscv64gc-unknown-none-elf/debug/deps/os-65d726c0a5683639.d
@@ -0,0 +1,5 @@
+/mnt/os/target/riscv64gc-unknown-none-elf/debug/deps/os-65d726c0a5683639.d: src/main.rs
+
+/mnt/os/target/riscv64gc-unknown-none-elf/debug/deps/os-65d726c0a5683639: src/main.rs
+
+src/main.rs:
